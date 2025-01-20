234314f88977b12c2d8896f4bacbeaf7dd1b3fbd
Dylan <voonichu@gmail.com>
2025-01-20 17:02:23 -0500

    [added stuff to cs file]

--- scripts/player.cs
+++ scripts/player.cs
@@ -3,4 +3,26 @@ using System;
 
 public partial class Player: CharacterBody3D
 {
+    [ExportSubgroup("Components")]
+    [Export]
+    public Node3D View;
+
+    [ExportSubgroup("Properties")]
+    [Export]
+    public int MovementSpeed  = 250;
+    [Export]
+    public int JumpStrength  = 7;
+
+
+    public Vector3 MovementVelocity;
+    public float RotationDirection;
+    public int Gravity = 0;
+
+    public bool PreviouslyFloored = false;
+
+    public bool JumpSingle = true;
+    public bool JumpDouble = true;
+
+    public int Coins = 0;
 }
+
