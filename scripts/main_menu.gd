c2c7cd1a0f5de25f03ec667652764880004b5b60
Dylan <dylanedwards3.14@gmail.com>
2025-03-13 13:37:45 -0400

    [added visual flair to main menu]

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,12 @@
-extends VBoxContainer
+extends Node
 
 
 const main = preload("res://scenes/main.tscn")
+var pivot
+@export var rotation_speed : float = 0.15
+
+func _ready():
+	pivot = $Pivot
 
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(main) 
@@ -11,3 +16,6 @@ func _on_button_2_pressed():
 
 func _on_button_3_pressed():
 	get_tree().quit()
+
+func _physics_process(delta):
+	pivot.global_rotation += Vector3(0, rotation_speed * delta, 0)
